7bdfe9261081f2763bff4711a403aabb65da139e
Bastian Inuk Christensen <jonalist@gmail.com>
2018-05-02 13:09:24 +0200

    commit

--- Package.swift
+++ Package.swift
@@ -1,32 +1,19 @@
 // swift-tools-version:4.0
+// Generated automatically by Perfect Assistant 2
+// Date: 2018-04-19 08:41:45 +0000
 import PackageDescription
 
 let package = Package(
-    name: "helloworld",
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
-
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc"),
-		
-		// 🖥 XML Parser for getting podcast
-		.package(url: "https://github.com/drmohundro/SWXMLHash", from: "4.0.0"),
-		.package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
-		
-		// 🖥 HTML Generator known as leaf.branch("beta")
-		.package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
-    ],
-    targets: [
-        .target(name: "App", dependencies: [
-			"FluentSQLite",
-			"Vapor",
-			"SWXMLHash",
-			"Alamofire",
-			"Leaf"
-		]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"]),
-    ]
+	name: "helloworld",
+	dependencies: [
+		.package(url: "https://github.com/vapor/vapor.git", "3.0.0-rc"..<"4.0.0"),
+		.package(url: "https://github.com/vapor/fluent-sqlite.git", "3.0.0-rc"..<"4.0.0"),
+		.package(url: "https://github.com/drmohundro/SWXMLHash", "4.0.0"..<"5.0.0"),
+		.package(url: "https://github.com/vapor/leaf.git", "3.0.0-rc"..<"4.0.0")
+	],
+	targets: [
+		.target(name: "App", dependencies: ["FluentSQLite", "Vapor", "SWXMLHash", "Leaf"]),
+		.target(name: "Run", dependencies: ["App"]),
+		.testTarget(name: "AppTests", dependencies: ["App"])
+	]
 )
-
